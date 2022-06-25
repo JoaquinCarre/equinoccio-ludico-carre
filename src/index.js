66096f56e3f35111800bb91c553fee854e875a62
Joaquín Carré <joa_carre21@hotmail.com>
2022-06-25 14:00:46 -0300

    subiendo prueba de app con ReactJS

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import logo from './miniEquinoccioLudicoLogo.png';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className='text-center'>
+      <img src={logo}></img>
+      <h1 className="fs-1 fw-bold">Editorial Equinoccio Ludico</h1>
+      <p className="fw-bold">Incentivados por hacer algo distinto en plena pandemia de Covid-19, un 21 de Septiembre de 2019, decidimos pasar de jugar juegos de mesa a crearlos. Y desde entonces comenzamos nuestro recorrido de aprendizaje, diversión, creación y esfuerzo para lograr crear piezas de arte que disfrutase cada jugón de tener en su ludoteca.</p>
+      <p className="fw-bold">El proceso es largo y aún estamos perfeccionando todos esos prototipos que esperan ser editados algún día. Mientras tanto creamos esta web para que puedan conocernos y estar al tanto de todas las novedades de la editorial.</p>
+      <p className="fw-bold">Esperamos que llegado el día nos acompañen a disfrutar de estos juegos desplegados en sus mesas. Estamos convencidos que 2022 será nuestro año, pero debemos seguir trabajando en ello.</p>
+      <p className="fw-bold">¡Gracias!</p>
+    </div>
   </React.StrictMode>
 );
 
