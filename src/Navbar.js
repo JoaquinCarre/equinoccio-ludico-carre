c075195a0ef4991ce6634e1a9536ef22171f8992
Joaquín Carré <joa_carre21@hotmail.com>
2022-06-26 15:59:20 -0300

    agregado de Navbar a la app web

---
+++ src/Navbar.js
@@ -0,0 +1,36 @@
+import './Navbar.css';
+import logo from './miniEquinoccioLudicoLogo.png';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <img src={logo} width={30} height={30}></img>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="">Inicio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled">Juegos</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled">Noticias</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled">Tienda</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import './index.css';
 import logo from './miniEquinoccioLudicoLogo.png';
+import Navbar from './Navbar';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className='text-center'>
+      <Navbar />
+    </div>
+    <div className='text-center container'>
       <img src={logo}></img>
       <h1 className="fs-1 fw-bold">Editorial Equinoccio Ludico</h1>
       <p className="fw-bold">Incentivados por hacer algo distinto en plena pandemia de Covid-19, un 21 de Septiembre de 2019, decidimos pasar de jugar juegos de mesa a crearlos. Y desde entonces comenzamos nuestro recorrido de aprendizaje, diversión, creación y esfuerzo para lograr crear piezas de arte que disfrutase cada jugón de tener en su ludoteca.</p>
